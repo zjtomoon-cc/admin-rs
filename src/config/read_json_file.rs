da080076986700a9b950ac9a5627fa4ea9eeec4e
zjtomoon <zjtomoon@outlook.com>
2023-02-26 10:23:26 +0800

    default commit

--- src/config/read_json_file.rs
+++ src/config/read_json_file.rs
@@ -12,19 +12,19 @@ pub struct File {
 }
 
 impl File {
-    fn new(name: &str) -> File {
+    pub fn new(name: &str) -> File {
         File {
             name:String::from(name),
             data:Vec::new(),
             state:FileState::Closed
         }
     }
-    fn new_with_data(name:&str,data:Vec<u8>) -> File {
+    pub fn new_with_data(name:&str,data:Vec<u8>) -> File {
         let mut f = File::new(name);
         f.data = data.clone();
         f
     }
-    fn read(self: &File,save_to:&mut Vec<u8>) -> Result<usize,String> {
+    pub fn read(self: &File,save_to:&mut Vec<u8>) -> Result<usize,String> {
         let mut tmp = self.data.clone();
         let read_length = tmp.len();
         save_to.reserve(read_length);
@@ -34,12 +34,16 @@ impl File {
     }
 }
 
-fn open(mut f:File) -> Result<File,String> {
+pub fn open(mut f:File) -> Result<File,String> {
     f.state = FileState::Open;
     Ok(f)
 }
 
-fn close(mut f:File) -> Result<File,String> {
+pub fn close(mut f:File) -> Result<File,String> {
     f.state = FileState::Closed;
     Ok(f)
 }
+
+/*
+关于公共函数和公共类，初始阶段先对每个函数公开，方便进行单元测试，后期使用统一的公共函数开放调用。
+ */
